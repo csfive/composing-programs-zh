cbfd528611130fa866575c141e782df691064433
mancuoj <1337381768@qq.com>
2023-05-01 23:33:36 +0800

    feat: update fonts

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,6 +2,8 @@ import DefaultTheme from 'vitepress/theme'
 import './rainbow.css'
 import './vars.css'
 import './overrides.css'
+import "@fontsource/noto-sans-sc"
+import "@fontsource/jetbrains-mono"
 
 export default {
     ...DefaultTheme
