3d12b38f20307122894840a2edd84303abcba5d6
mancuoj <1337381768@qq.com>
2023-05-05 23:24:57 +0800

    feat: giscus comment

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,23 @@
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 import './rainbow.css'
 import './vars.css'
 import './overrides.css'
 import "@fontsource/jetbrains-mono"
 
-export default { ...DefaultTheme }
+export default {
+  ...DefaultTheme,
+  setup() {
+    const { frontmatter } = useData();
+    const route = useRoute();
+    giscusTalk({
+      repo: 'csfive/composing-programs-zh',
+      repoId: 'R_kgDOI7ryQw',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOI7ryQ84CWSPL',
+    }, {
+      frontmatter, route
+    });
+  }
+}
