25ad00eef173592874bc7449aaccb54238c104a2
mancuoj <mancuoj@gmail.com>
2024-02-21 00:55:09 +0800

    chore: update

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,58 +1,82 @@
-import { sicp } from './sicp'
+import { sicp } from "./sicp"
 
 export default {
-  title: 'CS Five',
-  titleTemplate: ':title',
-  description: 'SICP Python 中文翻译',
-  lang: 'zh-CN',
+  title: "CS Five",
+  titleTemplate: ":title",
+  description: "SICP Python 中文翻译",
+  lang: "zh-CN",
   lastUpdated: true,
   cleanUrls: true,
-  srcDir: './sicp',
-  appearance: 'dark',
+  srcDir: "./sicp",
+  appearance: "dark",
   head: [
-    ['link', { rel: 'icon', href: '/logo.svg' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css' }],
-    ['meta', { name: 'keywords', content: 'SICP, Python, CS61A, Chinese Version, Translation, Composing Programs, 中文，翻译' }],
-    ['meta', { name: 'description', content: 'Composing Programs, aka SICP Python, Chinese version, 中文翻译版本' }],
-    ['meta', { name: 'author', content: 'CS Five' }],
+    ["link", { rel: "icon", href: "/logo.svg" }],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "SICP, Python, CS61A, Chinese Version, Translation, Composing Programs, 中文，翻译",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "description",
+        content:
+          "Composing Programs, aka SICP Python, Chinese version, 中文翻译版本",
+      },
+    ],
+    ["meta", { name: "author", content: "CS Five" }],
   ],
   markdown: {
     theme: {
-      light: 'vitesse-light',
-      dark: 'vitesse-dark',
+      light: "vitesse-light",
+      dark: "vitesse-dark",
     },
     config: (md) => {
-      md.use(require('markdown-it-smartarrows')).use(require('markdown-it-texmath'), {
-        engine: require('katex'),
-        delimiters: 'dollars',
-        katexOptions: { macros: { '\\RR': '\\mathbb{R}' } },
-      })
+      md.use(require("markdown-it-smartarrows")).use(
+        require("markdown-it-texmath"),
+        {
+          engine: require("katex"),
+          delimiters: "dollars",
+          katexOptions: { macros: { "\\RR": "\\mathbb{R}" } },
+        },
+      )
     },
   },
   themeConfig: {
-    logo: '/logo.svg',
+    logo: "/logo.svg",
     sidebar: {
-      '/': sicp,
+      "/": sicp,
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/csfive' },
-      { icon: 'discord', link: 'https://discord.gg/dqZaP5yAHT' },
+      { icon: "github", link: "https://github.com/csfive" },
+      { icon: "discord", link: "https://discord.gg/dqZaP5yAHT" },
     ],
-    outlineTitle: '大纲',
-    lastUpdatedText: '上次更新时间',
+    outlineTitle: "大纲",
+    lastUpdatedText: "上次更新时间",
     editLink: {
-      pattern: 'https://github.com/csfive/composing-programs-zh/edit/main/sicp/:path',
-      text: '在 GitHub 上修改此页',
+      pattern:
+        "https://github.com/csfive/composing-programs-zh/edit/main/sicp/:path",
+      text: "在 GitHub 上修改此页",
     },
     docFooter: {
-      prev: '上一页',
-      next: '下一页',
+      prev: "上一页",
+      next: "下一页",
     },
     footer: {
-      copyright: 'Copyright © 2022-PRESENT CS Five',
+      copyright: "Copyright © 2022-PRESENT CS Five",
     },
     search: {
-      provider: 'local',
+      provider: "local",
     },
   },
 }
