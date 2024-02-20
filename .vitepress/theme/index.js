5b8634386697d207176d82a68ece5ffdea9457d3
mancuoj <mancuoj@gmail.com>
2024-02-21 00:50:27 +0800

    chore: update

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,11 @@
-import DefaultTheme from 'vitepress/theme'
-import giscusTalk from 'vitepress-plugin-comment-with-giscus'
-import { useData, useRoute } from 'vitepress'
+import DefaultTheme from "vitepress/theme"
+import giscusTalk from "vitepress-plugin-comment-with-giscus"
+import Layout from "./Layout.vue"
+import { useData, useRoute } from "vitepress"
 
 export default {
   ...DefaultTheme,
+  Layout,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx)
   },
@@ -12,17 +14,17 @@ export default {
     const route = useRoute()
     giscusTalk(
       {
-        repo: 'csfive/composing-programs-zh',
-        repoId: 'R_kgDOI7ryQw',
-        category: 'Announcements',
-        categoryId: 'DIC_kwDOI7ryQ84CWSPL',
-        mapping: 'title',
-        strict: '1',
+        repo: "csfive/composing-programs-zh",
+        repoId: "R_kgDOI7ryQw",
+        category: "Announcements",
+        categoryId: "DIC_kwDOI7ryQ84CWSPL",
+        mapping: "title",
+        strict: "1",
       },
       {
         frontmatter,
         route,
-      }
+      },
     )
   },
 }
