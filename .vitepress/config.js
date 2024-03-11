62898e5dcde2947fac459a132b22696d6e4a7d85
mancuoj <mancuoj@gmail.com>
2024-03-11 23:34:51 +0800

    style: format

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -8,7 +8,6 @@ export default {
   lastUpdated: true,
   cleanUrls: true,
   srcDir: './sicp',
-  appearance: 'dark',
   head: [
     ['link', { rel: 'icon', href: '/logo.svg' }],
     [
@@ -37,10 +36,7 @@ export default {
     ['meta', { name: 'author', content: 'CS Five' }],
   ],
   markdown: {
-    theme: {
-      light: 'vitesse-light',
-      dark: 'vitesse-dark',
-    },
+    theme: { light: 'vitesse-light', dark: 'vitesse-dark' },
     config: (md) => {
       md.use(require('markdown-it-smartarrows')).use(
         require('markdown-it-texmath'),
@@ -54,13 +50,9 @@ export default {
   },
   themeConfig: {
     logo: '/logo.svg',
-    sidebar: {
-      '/': sicp,
-    },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/csfive' },
-      { icon: 'discord', link: 'https://discord.gg/dqZaP5yAHT' },
-    ],
+    sidebar: { '/': sicp },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/csfive' }],
+    search: { provider: 'local' },
     outlineTitle: '大纲',
     lastUpdatedText: '上次更新时间',
     editLink: {
@@ -68,15 +60,7 @@ export default {
         'https://github.com/csfive/composing-programs-zh/edit/main/sicp/:path',
       text: '在 GitHub 上修改此页',
     },
-    docFooter: {
-      prev: '上一页',
-      next: '下一页',
-    },
-    footer: {
-      copyright: 'Copyright © 2022-PRESENT CS Five',
-    },
-    search: {
-      provider: 'local',
-    },
+    docFooter: { prev: '上一页', next: '下一页' },
+    footer: { copyright: 'Copyright © 2022-PRESENT CS Five' },
   },
 }
